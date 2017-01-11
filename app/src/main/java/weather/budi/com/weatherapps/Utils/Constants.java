a65faaee05fae8318fcb07ee6a8850c0025ba8fd
budi <budi.viper@gmail.com>
2017-01-11 14:12:54 +0700

    init weather app

---
+++ app/src/main/java/weather/budi/com/weatherapps/Utils/Constants.java
@@ -0,0 +1,10 @@
+package weather.budi.com.weatherapps.Utils;
+
+/**
+ * Created by Budi on 1/11/2017.
+ */
+
+public class Constants {
+
+    public final static String APP_ID="7c8651deb1b33a65f059ec6acc4daa8f";
+}
